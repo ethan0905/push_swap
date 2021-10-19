c7aa1a27eddaaa452fc4477495b889ca56ead1dd
ethan0905 <77629339+ethan0905@users.noreply.github.com>
2021-10-19 14:56:46 +0200

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: esafar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 12:11:17 by esafar            #+#    #+#             */
-/*   Updated: 2021/10/11 16:59:57 by esafar           ###   ########.fr       */
+/*   Updated: 2021/10/19 14:50:05 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 
 void	initialize(t_data *data, int ac)
 {
-  data->ac = ac;
+	data->ac = ac;
 	data->count_tmp = 0;
 	data->remain_b = 0;
 	data->min = 0;
+	data->medA = 0;
+	data->medB = 0;
 	data->max = 0;
+	data->min_du_stack = 0;
+	data->max_du_stack = 0;
+	data->ra_tmp = 0;
+	data->rb_tmp = 0;
+	data->rrb_tmp = 0;
+	data->rra_tmp = 0;
+	data->ra = 0;
+	data->rb = 0;
+	data->rrb = 0;
+	data->rra = 0;
 }
 
-int	already_sorted(int *stackA, int size)
+int	already_sorted(long int *stackA, int size)
 {
 	int	i;
 
@@ -38,7 +50,7 @@ int	already_sorted(int *stackA, int size)
 	return (-1);
 }
 
-int check_tab(int *stackA, int size)
+int check_tab(long int *stackA, int size)
 {
 	int i;
 	int j;
@@ -59,7 +71,7 @@ int check_tab(int *stackA, int size)
 	return (1);
 }
 
-void	print_table(int *stackA, int *stackB)
+void	print_table(long int *stackA, long int *stackB)
 {
 	int i;
 	int j;
@@ -68,36 +80,53 @@ void	print_table(int *stackA, int *stackB)
 	int diff2;
 
 	i = 0;
-	printf("\n||            ||            ||\n");
-	while (stackA[i] != 0 || stackB[i] != 0)
+	printf("\n||                   ||                   ||\n");
+	while (stackA[i] != 3000000000 || stackB[i] != 3000000000)
 	{
-		diff = 7 - ft_strlen((ft_itoa(stackA[i])));
+		if (stackA[i] < 2999999999)
+			diff = 15 - ft_strlen((ft_itoa(stackA[i]))) - 1;
+		else if (stackA[i] == 3000000000)
+			diff = 15 - ft_strlen((ft_itoa(stackA[i])));
 		j = diff - 1;
-		printf("||    [%d]", stackA[i]);
+		printf("||    [%ld]", stackA[i]);
 		while (j-- > 0)
 			printf(" ");
 		printf("||");
-		printf("    [%d]", stackB[i]);
-		diff2 = 7 - ft_strlen((ft_itoa(stackB[i])));
-		j2 = diff2 - 1;
+		printf("    [%ld]", stackB[i]);
+		diff2 = 15 - ft_strlen((ft_itoa(stackB[i])));
+		j2 = diff2 - 2;
 		while (j2-- > 0)
 			printf(" ");
 		printf("||\n");
 		i++;
 	}
-	printf("==============================\n     STACK A       STACK B\n");
+	printf("===========================================\n       STACK A               STACK B\n");
+}
+
+void	b_pas_zero(long int *tab)
+{
+	int i;
+
+	i = 0;
+	while (i < 550)
+	{
+		tab[i] = 3000000000;
+		i++;
+	}
 }
 
 int	main(int ac, char **av)
 {
 	int	i;
-	int stackA[1000] = {};
-	int stackB[1000] = {};
+	long int stackA[1000] = {};
+	long int stackB[1000] = {};
 
 	i = 0;
+	b_pas_zero(stackA);
+	b_pas_zero(stackB);
 	while (i < ac - 1)
 	{
-		if (ft_atoi(av[i + 1]))
+		if (ft_atoi(av[i + 1]) || ft_atoi(av[i + 1]) == 0)
 			stackA[i] = ft_atoi(av[i + 1]);
 		else
 		{
@@ -113,8 +142,8 @@ int	main(int ac, char **av)
 	}
 	else if (ac == 1 || ac == 2 || already_sorted(stackA, (ac - 1)) == -1)
 		return (-1);
-//print_table(stackA, stackB);
+//	print_table(stackA, stackB);
 	solve(stackA, stackB, ac);
-//print_table(stackA, stackB);
+//	print_table(stackA, stackB);
 	return (0);
 }
